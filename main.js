3121febfb5e776848c062bc6d895e4f3fa3cd03d
Michael Kohler <mkohler@picobudget.com>
2015-04-28 01:54:39 +0200

    some design fixes

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ function showBox(id) {
 
 	var newBox = document.querySelector('#member-box-' + id);
 	newBox.classList.toggle('active');
+
+	var activePic = document.querySelector('.members-row img.active');
+	activePic.classList.toggle('active');
+
+	var picElem = document.querySelector('#pic' + id);
+	picElem.classList.toggle('active');
 }
