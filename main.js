f0521b35db75503fa8f7e3a354e817eaf350366e
Michael Kohler <mkohler@picobudget.com>
2015-06-05 21:14:20 +0200

    GA metrics (fixes #5)

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ function showBox(id) {
 	var picElem = document.querySelector('#pic' + id);
 	picElem.classList.toggle('active');
 }
+
+// Head 1
+document.querySelector('#head1').addEventListener('click', function(event) {
+	ga('send', 'event', 'link', 'click', 'story1');
+});
+
+// Head 2
+document.querySelector('#head2').addEventListener('click', function(event) {
+	ga('send', 'event', 'link', 'click', 'story2');
+});
+
+// Head 3
+document.querySelector('#head3').addEventListener('click', function(event) {
+	ga('send', 'event', 'link', 'click', 'story3');
+});
+
+// Head 4
+document.querySelector('#head4').addEventListener('click', function(event) {
+	ga('send', 'event', 'link', 'click', 'story4');
+});
+
+// Mitmachen Button
+document.querySelector('#ctalink').addEventListener('click', function(event) {
+	ga('send', 'event', 'link', 'click', 'ctabutton');
+});
+
+// Social media
+document.querySelector('#twitterLink').addEventListener('click', function(event) {
+	ga('send', 'event', 'link', 'click', 'twitterlink');
+});
+document.querySelector('#facebookLink').addEventListener('click', function(event) {
+	ga('send', 'event', 'link', 'click', 'facebooklink');
+});
